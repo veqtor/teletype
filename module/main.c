8e32fe2cc07a22bcb0068f0e6d8cd4cefdb7ce25
Sam Doshi <sam@metal-fish.co.uk>
2016-10-06 09:28:09 +0100

    clang-format

--- module/main.c
+++ module/main.c
@@ -1771,11 +1771,11 @@ void tele_ii(uint8_t i, int16_t d) {
     event_post(&e);
 }
 
-void tele_ii_tx(uint8_t addr, uint8_t *data, uint8_t l) {
+void tele_ii_tx(uint8_t addr, uint8_t* data, uint8_t l) {
     i2c_master_tx(addr, data, l);
 }
 
-void tele_ii_rx(uint8_t addr, uint8_t *data, uint8_t l) {
+void tele_ii_rx(uint8_t addr, uint8_t* data, uint8_t l) {
     i2c_master_rx(addr, data, l);
 }
 
