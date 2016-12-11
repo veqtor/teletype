9e22dc1798c7e5f43d7021c38f840ef7eed67013
Sam Doshi <sam@metal-fish.co.uk>
2016-12-11 16:21:20 +0000

    add test to check that ops and mods manipulate the stack correctly

--- tests/op_mod_tests.c
+++ tests/op_mod_tests.c
@@ -3,6 +3,7 @@
 #include "greatest/greatest.h"
 
 #include "ops/op.h"
+#include "teletype.h"
 
 // Check every op has a unique name
 TEST unique_ops() {
@@ -16,7 +17,7 @@ TEST unique_ops() {
     PASS();
 }
 
-// Check every op has a unique name
+// Check every mod has a unique name
 TEST unique_mods() {
     for (size_t i = 0; i < E_MOD__LENGTH; i++) {
         for (size_t j = 0; j < E_MOD__LENGTH; j++) {
@@ -28,7 +29,97 @@ TEST unique_mods() {
     PASS();
 }
 
+// Check every op manipulates the stack correctly
+TEST op_stack_size() {
+    for (size_t i = 0; i < E_OP__LENGTH; i++) {
+        const tele_op_t *op = tele_ops[i];
+
+        if (op->get != NULL) {
+            scene_state_t ss = {};  // initalise to empty
+                                    // (needs dedicated initaliser)
+            exec_state_t es;
+            es_init(&es);
+            command_state_t cs;
+            cs_init(&cs);
+
+            // add params to stack (plus an extra 2, to check that too many
+            // values aren't removed, warning: note the maximum stack size in
+            // state.h)
+            const int16_t stack_extra = 2;
+            for (int j = 0; j < op->params + stack_extra; j++) cs_push(&cs, 0);
+
+            // execute get
+            op->get(op->data, &ss, &es, &cs);
+
+            // check that the stack has the correct number of items in it
+            if (op->returns) {
+                ASSERT_EQm(op->name, cs_stack_size(&cs), stack_extra + 1);
+            }
+            else {
+                ASSERT_EQm(op->name, cs_stack_size(&cs), stack_extra);
+            }
+        }
+
+        if (op->set != NULL) {
+            scene_state_t ss = {};  // initalise to empty
+                                    // (needs dedicated initaliser)
+            exec_state_t es;
+            es_init(&es);
+            command_state_t cs;
+            cs_init(&cs);
+
+            // add params to stack (plus an extra 2, to check that too many
+            // values aren't removed, warning: note the maximum stack size in
+            // state.h)
+            // set functions require an extra value on the stack
+            const int16_t stack_extra = 2;
+            for (int j = 0; j < op->params + stack_extra + 1; j++)
+                cs_push(&cs, 0);
+
+            // execute get
+            op->set(op->data, &ss, &es, &cs);
+
+            // check that the stack has the correct number of items in it
+            ASSERT_EQm(op->name, cs_stack_size(&cs), stack_extra);
+        }
+    }
+    PASS();
+}
+
+// Check every mod manipulates the stack correctly
+TEST mod_stack_size() {
+    for (size_t i = 0; i < E_MOD__LENGTH; i++) {
+        const tele_mod_t *mod = tele_mods[i];
+
+        scene_state_t ss = {};  // initalise to empty
+                                // (needs dedicated initaliser)
+        exec_state_t es;
+        es_init(&es);
+        command_state_t cs;
+        cs_init(&cs);
+
+        // add params to stack (plus an extra 2, to check that too many
+        // values aren't removed, warning: note the maximum stack size in
+        // state.h)
+        const int16_t stack_extra = 2;
+        for (int j = 0; j < mod->params + stack_extra; j++) cs_push(&cs, 0);
+
+        // execute func
+        const tele_command_t sub_command = {.length = 1,
+                                            .separator = 0,
+                                            .data = { {.tag = OP,
+                                                       .value = E_OP_A } } };
+        mod->func(&ss, &es, &cs, &sub_command);
+
+        // check that the stack has the correct number of items in it
+        ASSERT_EQm(mod->name, cs_stack_size(&cs), stack_extra);
+    }
+    PASS();
+}
+
 SUITE(op_mod_suite) {
     RUN_TEST(unique_ops);
     RUN_TEST(unique_mods);
+    RUN_TEST(op_stack_size);
+    RUN_TEST(mod_stack_size);
 }
