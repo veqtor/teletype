38aabfcf458d8204578285a9a3d292659939999a
Sam Doshi <sam@metal-fish.co.uk>
2017-04-15 09:08:51 +0100

    remove commented code from hardware.c

--- src/ops/hardware.c
+++ src/ops/hardware.c
@@ -67,9 +67,6 @@ const tele_op_t op_STATE    = MAKE_GET_OP    (STATE   , op_STATE_get   , 1, true
 static void op_CV_get(const void *NOTUSED(data), scene_state_t *ss,
                       exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
-    // cs_push(cs, ss->variables.cv[a]);
-
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -92,11 +89,7 @@ static void op_CV_set(const void *NOTUSED(data), scene_state_t *ss,
                       exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
     b = normalise_value(0, 16383, 0, b);
-    // ss->variables.cv[a] = b;
-    // tele_cv(a, b, 1);
-
     a--;
     if (a < 0)
         return;
@@ -115,9 +108,6 @@ static void op_CV_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_CV_SLEW_get(const void *NOTUSED(data), scene_state_t *ss,
                            exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
-    // cs_push(cs, ss->variables.cv_slew[a]);
-
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -140,11 +130,7 @@ static void op_CV_SLEW_set(const void *NOTUSED(data), scene_state_t *ss,
                            exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
     b = normalise_value(1, 32767, 0, b);  // min slew = 1
-    // ss->variables.cv_slew[a] = b;
-    // tele_cv_slew(a, b);
-
     a--;
     if (a < 0)
         return;
@@ -162,9 +148,6 @@ static void op_CV_SLEW_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_CV_OFF_get(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
-    // cs_push(cs, ss->variables.cv_off[a]);
-
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -187,11 +170,7 @@ static void op_CV_OFF_set(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, CV_COUNT - 1, 0, a - 1);
     ss->variables.cv_off[a] = b;
-    // tele_cv_off(a, b);
-    // tele_cv(a, ss->variables.cv[a], 1);
-
     a--;
     if (a < 0)
         return;
@@ -220,7 +199,6 @@ static void op_PARAM_get(const void *NOTUSED(data), scene_state_t *ss,
 static void op_TR_get(const void *NOTUSED(data), scene_state_t *ss,
                       exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -242,7 +220,6 @@ static void op_TR_set(const void *NOTUSED(data), scene_state_t *ss,
                       exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
     a--;
     if (a < 0)
         return;
@@ -260,8 +237,6 @@ static void op_TR_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_TR_POL_get(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
-    // cs_push(cs, ss->variables.tr_pol[a]);
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -283,8 +258,6 @@ static void op_TR_POL_set(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
-    // ss->variables.tr_pol[a] = b > 0;
     a--;
     if (a < 0)
         return;
@@ -301,8 +274,6 @@ static void op_TR_POL_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_TR_TIME_get(const void *NOTUSED(data), scene_state_t *ss,
                            exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
-    // cs_push(cs, ss->variables.tr_time[a]);
     a--;
     if (a < 0)
         cs_push(cs, 0);
@@ -325,9 +296,7 @@ static void op_TR_TIME_set(const void *NOTUSED(data), scene_state_t *ss,
                            exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // a = normalise_value(0, TR_COUNT - 1, 0, a - 1);
     if (b < 0) b = 0;
-    // ss->variables.tr_time[a] = b;
     a--;
     if (a < 0)
         return;
@@ -344,14 +313,6 @@ static void op_TR_TIME_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_TR_TOG_get(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // saturate and shift
-    // a--;
-    // if (ss->variables.tr[a])
-    //     ss->variables.tr[a] = 0;
-    // else
-    //     ss->variables.tr[a] = 1;
-    // tele_tr(a, ss->variables.tr[a]);
-
     a--;
     if (a < 0)
         return;
@@ -372,19 +333,6 @@ static void op_TR_TOG_get(const void *NOTUSED(data), scene_state_t *ss,
 static void op_TR_PULSE_get(const void *NOTUSED(data), scene_state_t *ss,
                             exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // saturate and shift
-    // if (a < 1)
-    //     a = 1;
-    // else if (a > 4)
-    //     a = 4;
-    // a--;
-    // int16_t time = ss->variables.tr_time[a];  // pulse time
-    // if (time <= 0) return;                    // if time <= 0 don't do
-    // anything
-    // ss->variables.tr[a] = ss->variables.tr_pol[a];
-    // ss->tr_pulse_timer[a] = time;  // set time
-    // tele_tr(a, ss->variables.tr[a]);
-
     a--;
     if (a < 0)
         return;
@@ -406,18 +354,11 @@ static void op_CV_SET_get(const void *NOTUSED(data), scene_state_t *ss,
                           exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
     int16_t b = cs_pop(cs);
-    // saturate and shift
-    // if (a < 1)
-    //     a = 1;
-    // else if (a > 4)
-    //     a = 4;
-    // a--;
+
     if (b < 0)
         b = 0;
     else if (b > 16383)
         b = 16383;
-    // ss->variables.cv[a] = b;
-    // tele_cv(a, b, 0);
 
     a--;
     if (a < 0)
@@ -452,14 +393,6 @@ static void op_MUTE_set(const void *NOTUSED(data), scene_state_t *ss,
 static void op_STATE_get(const void *NOTUSED(data), scene_state_t *NOTUSED(ss),
                          exec_state_t *NOTUSED(es), command_state_t *cs) {
     int16_t a = cs_pop(cs);
-    // a--;
-    // if (a < 0)
-    //     a = 0;
-    // else if (a > 7)
-    //     a = 7;
-
-    // cs_push(cs, tele_get_input_state(a));
-
     a--;
     if (a < 0)
         cs_push(cs, 0);
